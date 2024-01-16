430ef9173579434261a6103384ab87ee8e9c57d4
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-01-16 17:56:43 +0600

    feat: add button & input ui

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,20 +1,24 @@
 import { cn } from "@/lib/utils";
-import React from "react";
+import * as React from "react";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export function Button({ children, className, ...rest }: ButtonProps) {
-  return (
-    <button
-      {...rest}
-      className={cn(
-        "rounded-md border border-tg-btn-color px-2 text-sm text-tg-btn-color hover:bg-tg-btn-color hover:text-tg-btn-text-color",
-        className,
-      )}
-    >
-      {children}
-    </button>
-  );
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <button
+        className={cn(
+          "hover:bg-tg-btn-color/90 inline-flex items-center justify-center whitespace-nowrap rounded-md bg-tg-btn-color px-3 py-2 text-xs font-medium text-tg-btn-text-color shadow transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  },
+);
+Button.displayName = "Button";
+
+export default Button;
---
+++ src/app/components/Input.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import * as React from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-md border border-tg-btn-color bg-tg-secondary-bg-color px-3 py-1 text-sm ring-tg-btn-color transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-tg-hint-color focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+
+Input.displayName = "Input";
+
+export default Input;
